711c0ce9909fd2d0c82ceedcac873674c93cf53d
okok0415 <dlawjdals11@naver.com>
2021-11-18 13:17:13 +0900

    #1.1 Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
+import { createStore } from "redux"
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span")
 
-let count = 0;
+const countModifier = (state = 0) => {
+  return state;
+};
 
-const updateText = () => {
-  number.innerText = count;
-}
+const countStore = createStore(countModifier);
 
-const handleAdd = () => {
-  count += 1;
-  updateText();
-}
-const handleMinus = () => {
-  count -= 1;
-  updateText();
-}
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+console.log(countStore.getState());
