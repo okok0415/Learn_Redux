1732dc4639cabab8414f54036401fe514296420d
okok0415 <dlawjdals11@naver.com>
2021-11-19 22:20:08 +0900

    # git

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const DELETE_TODO = "DELETE_TODO"
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return []
+      return [...state, { text: action.text, id: Date.now() }]
     case DELETE_TODO:
       return [];
     default:
@@ -18,9 +18,25 @@ const reducer = (state = [], action) => {
 
 const store = createStore(reducer);
 
+store.subscribe(() => console.log(store.getState()))
 
+const paintToDos = () => {
+  const toDos = store.getState();
+  toDos.forEach(toDo => {
+    const li = document.createElement("li")
+    li.id = toDo.id
+    li.innerText = toDo.text;
+    ul.appendChild
+  })
+}
+store.subscribe(paintToDos)
 
-const createToDo = todo => {
+const addToDo = (text) => {
+  store.dispatch({ type: ADD_TODO, text: toDo })
+}
+const
+
+const createToDo = toDo => {
   const li = document.createElement("li")
   li.innerText = toDo;
   ul.appendChild(li)
@@ -30,7 +46,7 @@ const onSubmit = e => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  store.dispatch({ type: ADD_TODO, text: toDo })
+  addToDo();
 };
 
 form.addEventListener("submit", onSubmit)
